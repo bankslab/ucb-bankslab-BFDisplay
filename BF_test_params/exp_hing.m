f39ddbf3f45750dffa7dd8ea5edf13f966d561e0
BF display control <BFdisplay@BFs-Mac-Pro.local>
2013-10-29 14:39:22 -0700

    Nothing new in this commit. Fourth Data File (dissociated focus and disparity cues)

--- BF_test_params/exp_hing.m
+++ BF_test_params/exp_hing.m
@@ -18,10 +18,10 @@ param.stim_duration = 1;
 param.algorithm = {'optimization', 'blending', 'single', 'sharp'};
 %param.algorithm = {'optimization', 'sharp'};
 param.hinge_dist = 2;%[2 : 1.2 : 3.2];
-param.focus_dist = 2;%[2 : 1.2 : 3.2];
-param.MCS_stimuli = [78 : 3 : 102]; %[60 : 3 : 120]; %angle values
+param.focus_dist = [2, 3.2];%[2 : 1.2 : 3.2];
+param.MCS_stimuli = [75 : 3 : 105]; %[60 : 3 : 120]; %angle values
 param.angle_noise = [0 0];%[-5 0 5];
-param.max_responses = 10;
+param.max_responses = 5;
 
 % count how many staircases we want
 
@@ -31,20 +31,19 @@ scell{1} = set(s,...
 
 for alg_index = 1:length(param.algorithm)
     for hinge_dist_index = 1:length(param.hinge_dist)
-        %for focus_index = 1:length(param.focus_dist)
-            scell{alg_index, hinge_dist_index} = set(scell{1},... %scell{alg_index, hinge_dist_index, focus_index} = set(scell{1},...
+        for focus_index = 1:length(param.focus_dist)
+            scell{alg_index, hinge_dist_index, focus_index} = set(scell{1},...
                 'algorithm',param.algorithm{alg_index},...
                 'hinge_distance',param.hinge_dist(hinge_dist_index),...
-                'focus_distance', param.hinge_dist(hinge_dist_index),... %'focus_distance', param.focus_dist(focus_index),...
+                'focus_distance', param.focus_dist(focus_index),...
                 'MCS_stimuli', param.MCS_stimuli,... %will need to be more specific for 2nd test
                 'angle_noise', param.angle_noise(randi(2)),...
                 'MCS_num_responses',zeros(1,length(param.MCS_stimuli)),...
                 'MCS_num_stimuli',length(param.MCS_stimuli),...
                 'MCS_max_responses', param.max_responses);
-            scell{alg_index, hinge_dist_index}=... %scell{alg_index, hinge_dist_index, focus_index}=...
-                initializeStaircase(scell{alg_index, hinge_dist_index});
-                %initializeStaircase(scell{alg_index, hinge_dist_index, focus_index});
-        %end
+            scell{alg_index, hinge_dist_index, focus_index}=...
+                initializeStaircase(scell{alg_index, hinge_dist_index, focus_index});
+        end
     end
 end
 
