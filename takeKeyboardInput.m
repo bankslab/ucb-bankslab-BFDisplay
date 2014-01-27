f0b4442f340d57842fbee0ce6a5281eaa1034398
Rachel Albert <rachelalbert@berkeley.edu>
2014-01-27 15:21:42 -0800

    Changes to keyboard input and saving code. (1) Renamed processResponse to processResponseStaircase (2) Added takeKeyboardInput This file Gets Input and Changes Displayed Image (3) Modified process_response We added data saving for both scell and text file It also process the response for both MCS and staircase It also opens the data file and prints a header

---
+++ takeKeyboardInput.m
@@ -0,0 +1,36 @@
+% This script handles all keyboard input for the BF display
+% It Gets Input and Changes Displayed Image
+% 
+% Example 1: Dynamically change the displayed image
+% if strcmp(strInputName,'1!')
+%     targetPosition = targetPosition + 1;
+% end % Now BF_build_textures_optimizer will redraw the target
+% 
+%
+% Example 2: Continue with response
+% if strcmp(strInputName, 'SPACE')
+%     response_given = 1;
+%     break;
+% end % Now it will continue to the next trial
+
+
+iKeyIndex = find(c);
+strInputName = KbName(iKeyIndex(1));
+if strcmp(strInputName,'1!')
+    show_image = 1;
+elseif strcmp(strInputName,'2@')
+    show_image = 2;
+elseif strcmp(strInputName,'3#')
+elseif strcmp(strInputName,'4$')
+elseif strcmp(strInputName, 'RightArrow')
+elseif strcmp(strInputName, 'LeftArrow')
+elseif strcmp(strInputName, 'UpArrow')
+elseif strcmp(strInputName, 'DownArrow')
+elseif strcmp(strInputName, 'SPACE')
+elseif strcmp(strInputName, 'Return')
+    response_given = 1;
+    break;
+elseif strcmp(strInputName,'ESCAPE')||strcmp(strInputName,'esc')
+    stop_flag = 1;
+    break;
+end
