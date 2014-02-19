aea4d9bb3034d1e5e2c97b03859ae0b7879abdee
BF display control <BFdisplay@babyvision.berkeley.edu>
2014-02-18 18:54:27 -0800

    Removed old code. (No functional changes)

--- BF_run_trial.m
+++ BF_run_trial.m
@@ -1,46 +1,44 @@
 % This script displays the pre-loaded GL commands and waits for input
 
-if strcmp(experiment_type, 'comparison')
-    a = 0;
-    while a == 0
-        [a b c d] = KbCheck(-1);
-        depthplane = depthplane + 1;
-        if depthplane > 4
-            depthplane = 1;
-        end
-        depthtex_handle = depthplane;
-        for whichEye = renderviews
-            Screen('SelectStereoDrawBuffer', windowPtr, whichEye);
-            Screen('BeginOpenGL', windowPtr);
-            if whichEye == 0
-                glCallList(genlist_projection1(depthplane+whichEye*4));    %mandatory projection setup
-                if static_mode  %optional mode for staic imagery
-                    glCallList(static_scene_disp_list1(depthplane+whichEye*4));
-                end
-            else
-                glCallList(genlist_projection1(depthplane+whichEye*4));    %mandatory projection setup
-                if static_mode  %optional mode for staic imagery
-                    glCallList(static_scene_disp_list1(depthplane+whichEye*4));
-                end
-            end
-            Screen('EndOpenGL', windowPtr);
-            
-            if depthplane==3
-                if whichEye==1
-                    Screen('FillRect', windowPtr, [255 255 255], [winRect(3)*.85, winRect(4)*.85, winRect(3) , winRect(4)]);
-                else
-                    Screen('FillRect', windowPtr, [255 255  255], [0, winRect(4)*.85, winRect(3)*.15 , winRect(4)]);
-                end
-            else
-                if whichEye==1
-                    Screen('FillRect', windowPtr, [0 0 0], [winRect(3)*.85, winRect(4)*.85, winRect(3) , winRect(4)]);
-                else
-                    Screen('FillRect', windowPtr, [0 0 0], [0, winRect(4)*.85, winRect(3)*.15 , winRect(4)]);
-                end
-            end
-        end
-        % TRY REMOVING THIS LINE
-        Screen('Flip', windowPtr, [], 2, 1);
+a = 0;
+while a == 0
+    [a b c d] = KbCheck(-1);
+    depthplane = depthplane + 1;
+    if depthplane > 4
+        depthplane = 1;
     end
+    depthtex_handle = depthplane;
+    for whichEye = renderviews
+        Screen('SelectStereoDrawBuffer', windowPtr, whichEye);
+        Screen('BeginOpenGL', windowPtr);
+        if whichEye == 0
+            glCallList(genlist_projection1(depthplane+whichEye*4));    %mandatory projection setup
+            if static_mode  %optional mode for staic imagery
+                glCallList(static_scene_disp_list1(depthplane+whichEye*4));
+            end
+        else
+            glCallList(genlist_projection1(depthplane+whichEye*4));    %mandatory projection setup
+            if static_mode  %optional mode for staic imagery
+                glCallList(static_scene_disp_list1(depthplane+whichEye*4));
+            end
+        end
+        Screen('EndOpenGL', windowPtr);
+        
+        if depthplane==3
+            if whichEye==1
+                Screen('FillRect', windowPtr, [255 255 255], [winRect(3)*.85, winRect(4)*.85, winRect(3) , winRect(4)]);
+            else
+                Screen('FillRect', windowPtr, [255 255  255], [0, winRect(4)*.85, winRect(3)*.15 , winRect(4)]);
+            end
+        else
+            if whichEye==1
+                Screen('FillRect', windowPtr, [0 0 0], [winRect(3)*.85, winRect(4)*.85, winRect(3) , winRect(4)]);
+            else
+                Screen('FillRect', windowPtr, [0 0 0], [0, winRect(4)*.85, winRect(3)*.15 , winRect(4)]);
+            end
+        end
+    end
+    % TRY REMOVING THIS LINE
+    Screen('Flip', windowPtr, [], 2, 1);
 end
 onset=Screen('Flip', windowPtr, [], []);
